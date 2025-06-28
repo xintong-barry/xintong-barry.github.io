cedfd5b0d8c29fd848d79e24072b30168c1725a4
xintong-barry <xintong.barry@gmail.com>
2025-06-28 04:19:51 +0000

    Deploying to gh-pages from @ xintong-barry/xintong-barry.github.io@2ebac0e7fbe8840a8f97db44dda29e3d8268a226 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "Publications",
-          description: "publications by categories in reversed chronological order. * denotes equal contributions.",
+          description: "Publications by categories in reversed chronological order. * denotes equal contributions.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
