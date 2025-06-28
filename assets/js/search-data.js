7f499b0f6d065dcfea65f6bdc78a44b60db1328a
xintong-barry <xintong.barry@gmail.com>
2025-06-28 09:02:05 +0000

    Deploying to gh-pages from @ xintong-barry/xintong-barry.github.io@0e3040029019514420ce677fb1cc196cf5f981f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My interactive resume can be found below, while the full version can be accessed [here]({{ site.baseurl }}/assets/pdf/CV_XinTong_20250626.pdf).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
