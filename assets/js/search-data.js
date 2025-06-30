25f385e4d864a75bce8460bc9200aa83b4a2edd6
xintong-barry <xintong.barry@gmail.com>
2025-06-30 08:14:43 +0000

    Deploying to gh-pages from @ xintong-barry/xintong-barry.github.io@7d2c71e011c70392faa2b3abb663da99589aba28 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "A summary of my research interests.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "The PDF version of my CV can be found via the button on the right.",
